64123b48a54b6fcd589d48a2e351960f400b2e37
Yves Raimond <yves.raimond@bbc.co.uk>
2010-09-04 15:17:02 +0100

    Messy, but sends POST

--- gen/org/moustaki/trackdropper/R.java
+++ gen/org/moustaki/trackdropper/R.java
@@ -12,10 +12,13 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
-        public static final int self=0x7f020001;
+        public static final int music=0x7f020001;
+        public static final int self=0x7f020002;
     }
     public static final class id {
-        public static final int mapview=0x7f050000;
+        public static final int EditMessage=0x7f050002;
+        public static final int layout_root=0x7f050000;
+        public static final int mapview=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/org/moustaki/trackdropper/TrackDropper.java
+++ src/org/moustaki/trackdropper/TrackDropper.java
@@ -1,25 +1,53 @@
 package org.moustaki.trackdropper;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.database.Cursor;
 import android.graphics.drawable.Drawable;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.moustaki.trackdropper.R;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 
+import android.provider.MediaStore.Audio.Media;
+
 public class TrackDropper extends MapActivity {
     
-    private static final int MENU_ADD_OBJECTIVES = 0;
+    private static final int MENU_DROP_TRACK = 0;
+    private static final int MENU_QUIT = 1;
     
     private MapController mc;
     private MapView mv;
     private LocationManager lm;
     private TrackDropperLocationListener ll;
+    private boolean running;
+    
+    private String base = "http://10.195.80.235:6666";
     
     /** Called when the activity is first created. */
     @Override
@@ -40,6 +68,8 @@ public class TrackDropper extends MapActivity {
         this.mv.getOverlays().add(selfOverlay);
         this.ll = new TrackDropperLocationListener(this, selfOverlay);
         this.lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
+        
+        this.running = true;
     }
 
     @Override
@@ -62,6 +92,139 @@ public class TrackDropper extends MapActivity {
      
      public boolean onPrepareOptionsMenu(Menu menu) {
          menu.clear();
+         menu.add(0, MENU_DROP_TRACK, 0, "Drop track!");
+         menu.add(0, MENU_QUIT, 0, "Quit");
         return true;
+     }
+     
+     public boolean onOptionsItemSelected(MenuItem item) {
+         switch (item.getItemId()) {
+             case MENU_QUIT:
+                 this.running = false;
+                 this.finish();
+                 return true;
+             case MENU_DROP_TRACK:
+                 this.chooseArtist();
+                 return true;
+         }
+         return false;
+     }
+     
+     private boolean chooseArtist() {
+         String[] projection = new String[] {
+                 Media._ID,
+                 Media.TITLE,
+                 Media.DATA,
+                 Media.ARTIST,
+                 Media.ALBUM,
+              };
+         Uri music = Media.EXTERNAL_CONTENT_URI;
+         Cursor cursor = managedQuery(music, projection, null, null, Media.TITLE + " ASC");
+         ArrayList<String> titles = new ArrayList<String>();
+         ArrayList<String> artists = new ArrayList<String>();
+         if (cursor.moveToFirst()) {
+             int titleColumn = cursor.getColumnIndex(Media.TITLE);
+             int artistColumn = cursor.getColumnIndex(Media.ARTIST);
+             do {
+                 titles.add(cursor.getString(titleColumn));
+                 artists.add(cursor.getString(artistColumn));
+             } while (cursor.moveToNext());
+         }
+         final ArrayList<String> finaltitles =  titles;
+         final ArrayList<String> finalartists = artists;
+         AlertDialog.Builder builder = new AlertDialog.Builder(this);
+         builder.setTitle("Pick a track:");
+         String[] s = (String[]) titles.toArray(new String[titles.size()]);
+         builder.setItems(s, new DialogInterface.OnClickListener() {
+             public void onClick(DialogInterface dialog, int item) {
+                 String title = finaltitles.get(item);
+                 String artist = finalartists.get(item);
+                 GeoPoint point = getLocationListener().getCurrentLocation();
+                 HashMap<String,String> data = new HashMap<String,String>();
+                 
+                 data.put("lat", ""+point.getLatitudeE6());
+                 data.put("lng", ""+point.getLongitudeE6());
+                 data.put("artist_name", artist);
+                 data.put("track_name", title);
+                 postJSON("/tracks", data);
+                 Toast.makeText(getTrackDropper(), "Posted!", Toast.LENGTH_SHORT).show();
+                 return;
+             }
+         });
+         AlertDialog alert = builder.create();
+         alert.show();
+         return true;
+     }
+     
+     public void getNearbyTracks() {
+         Thread getNearbyTracks = new Thread() {
+             public void run() {
+                 try {
+                     while (running) {
+                         Thread.sleep(1000 * 10);
+                     }
+                 } catch (InterruptedException e) {
+                     e.printStackTrace();
+                 }
+             }
+         };
+         getNearbyTracks.start();
+         return;
+     }
+     
+     public TrackDropper getTrackDropper() {
+         return this;
+     }
+     
+     private JSONObject getJSON(String path) {
+         try {
+             URL url = new URL(this.base + path);
+             URLConnection connection = url.openConnection();
+             String line;
+             StringBuilder builder = new StringBuilder();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+             while((line = reader.readLine()) != null) {
+                 builder.append(line);
+             }
+             String response = builder.toString();
+             return new JSONObject(response);
+         } catch (IOException e) {
+             e.printStackTrace();
+         } catch (JSONException e) {
+             e.printStackTrace();
+         }
+         return null;
+     }
+     
+     private JSONObject postJSON(String path, HashMap<String,String> data) {
+         try {
+             String post = "";
+             for (String key : data.keySet()) {
+                 if (post != "") {
+                     post += "&";
+                 }
+                 post += URLEncoder.encode(key, "UTF-8") + '=' + URLEncoder.encode(data.get(key), "UTF-8");
+             }
+             URL url = new URL(this.base + path);
+             URLConnection connection = url.openConnection();
+             connection.setDoOutput(true);
+             OutputStreamWriter wr = new OutputStreamWriter(connection.getOutputStream()); 
+             wr.write(post);
+             wr.flush();
+             
+             String line;
+             StringBuilder builder = new StringBuilder();
+             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+             while((line = reader.readLine()) != null) {
+                 builder.append(line);
+             }
+             String response = builder.toString();
+             return new JSONObject(response);
+         } catch (IOException e) {
+             e.printStackTrace();
+         } catch (JSONException e) {
+             e.printStackTrace();
+         }
+         return null;
      }
 }
